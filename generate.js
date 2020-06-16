1e46ce579f48e8b6e4a496585c870fb2e534d6f0
Ivo Georgiev <ivo@strem.io>
2020-06-16 10:16:43 +0200

    generate almost done

--- generate.js
+++ generate.js
@@ -13,21 +13,42 @@ async function generateAddon(yamlPath, addonPath) {
 	assert.ok(doc.addon, 'has addon field')
 	assert.ok(doc.catalog, 'has catalog')
 	const cat = doc.catalog
+	const id = `com.autogen.${cat.type}`
 	await writeToFile(path.join(addonPath, 'manifest.json'), {
-		id: `com.autogen.${cat.type}`,
+		id,
 		name: doc.addon.name,
 		version: doc.addon.version,
 		description: doc.addon.description || '',
 		resources: ['meta'],
 		types: [cat.type],
+		idPrefixes: [`${id}:`],
 		catalogs: [{ type: cat.type, id: 'main', name: doc.addon.name }]
 	})
-	await Promise.all(Object.entries(cat.items).map(([name, items]) => {
-		const videos = items.filter(x => typeof x === 'string' && x.includes('youtube.com'))
-		if (!videos.length) throw new Error(`catalog ${name} does not have any youtube videos listed`)
+	const catEntries = Object.entries(cat.items)
+	const metas = catEntries.map(([name, items]) => {
 		const thumbEntry = items.find(x => x.thumb)
 		const thumb = thumbEntry ? thumbEntry.thumb : videoToThumb(videos[0])
-		console.log(name, videos.map(videoToThumb), thumb)
+		return {
+			id: `${id}:${name}`,
+			name,
+			poster: thumb,
+			posterShape: 'landscape',
+			background: thumb,
+			type: cat.type,
+		}
+	})
+	await writeToFile(path.join(addonPath, `catalog/${cat.type}/main.json`), { metas })
+	await Promise.all(catEntries.map(([name, items], i) => {
+		const videos = items.filter(x => typeof x === 'string' && x.includes('youtube.com'))
+		if (!videos.length) throw new Error(`catalog ${name} does not have any youtube videos listed`)
+		const metaId = `${id}:${name}`
+		return writeToFile(path.join(addonPath, `meta/${cat.type}/${metaId}.json`), {
+			meta: {
+				...metas[i],
+				videos: videos.map(videoToObject)
+			}
+		})
+		
 	}))
 }
 
@@ -40,6 +61,16 @@ function videoToThumb(videoUrl) {
 	const { v } = url.parse(videoUrl, true).query
 	return `https://i.ytimg.com/vi/${v}/hqdefault.jpg`
 }
+function videoToObject(videoUrl) {
+	const { v } = url.parse(videoUrl, true).query
+	return {
+		id: v,
+		title: '', // @TODO
+		streams: [{ yt_id: v }],
+		thumbnail: `https://i.ytimg.com/vi/${v}/hqdefault.jpg`,
+	}
+}
+
  
 generateAddon('./kyuchek.yaml', './dist')
 	.then(() => console.log('Generation complete!'))
